4ef0855d5ac83931f0c874477fb2d616ee55c837
Ali EL KHALIL <ali.el-khalil@ipsa.fr>
2021-05-04 02:47:25 +0200

    Fin de la partie 3.
    Ajout d'un programme principal qui répond aux questions de la partie 3 concernant la résolution numérique.

--- Partie_3.py
+++ Partie_3.py
@@ -24,8 +24,8 @@ import matplotlib.pyplot as plt
 from math import *
 
 #-----------------------------------------------
-
-h=10**(-3)
+#On fixe un pas pour toute la partie.
+h=10**-3
 
 #-----------------------------------------------
 #Question 2.(a)
@@ -46,40 +46,80 @@ def Mat(a, b, K, f):
         A[i,0]=K(t[i],t[0])
         A[i,N]=K(t[i],t[N])
         for j in range(1,N):
-            2*K(t[i],t[j])
+            A[i,j]=2*K(t[i],t[j])
     #calcul de M
     M=np.eye(N+1)-(h/2)*A
     return t,F,M
 
-def resolution(a,b,K,f):
+#-----------------------------------------------
+#question 2.(c)
+#-----------------------------------------------
+
+def resolution(a,b,K,f, figure=False,titre=False):
     t,F,M=Mat(a,b,K,f)
     U=np.linalg.solve(M,F) #Résolution du système MU=F
-    return U,t
+    if (figure):
+        plt.plot(t,U)
+        plt.xlabel("t")
+        plt.ylabel("y(t)")
+        if (titre):
+            plt.title(titre)
+        else : 
+            plt.title("Courbe de U, solution calculée")
+        plt.show()
+    return U,t 
 
-def comparaison(a,b,K,f,V, figure=False):
+#-----------------------------------------------
+#question 2.(d)
+#-----------------------------------------------
+
+def comparaison(a,b,K,f,V, figure=False, inf=False):
     U,t=resolution(a,b,K,f)
     Vtab=V(t)
     if (figure):
-        plt.plot(t,U,label="Tracé de U (solution calculée)")
+        plt.plot(t,U,label="Tracé de U (solution calculée)", marker='x')
         plt.plot(t,Vtab,label="Tracé de V (solution exacte)")
         plt.xlabel("t")
         plt.ylabel("y(t)")
         plt.title("Courbe des solutions de l'équation intégrable")
         plt.legend()
         plt.show()
-    Erreur=np.linalg.norm(U-Vtab)
+    if (inf):
+        Erreur=np.linalg.norm(U-Vtab, np.inf)
+    else :
+        Erreur=np.linalg.norm(U-Vtab)
     return Erreur
+
+
+if __name__ == '__main__':
+    #sous-partie 3.2
+    print("Partie 3.2 : Présentation de benchmark utilisée")
+    def K(x,t):
+        return 2
+    def f(x):
+        return np.cos(np.pi*x/2)-8/np.pi
+    def V(x):
+        return np.cos(np.pi*x/2)
+    a=-1
+    b=1
+    resolution(a,b,K,f, figure=True)
+    E2=comparaison(a,b,K,f,V,figure=True)
+    Einf=comparaison(a,b,K,f,V,inf=True)
+    print ("Question 2. (d) :")
+    print("L'erreur en norme 2 est : ||U-V||_2 = "+str(E2))
+    print("L'erreur en norme infinie est : "+str(Einf))
+    print("Fin de la partie 3.2")
+    #sous-partie 3.3
+    print("Partie 3.3 : Équation de Love en électrostatique")
+    def K(x,t):
+        return 1/(np.pi*(1+(x-t)**2))
+    def f(x):
+        return 1
+    U,t=resolution(a,b,K,f,figure=True, titre="Courbe de U, solution de l'équation de Love")
+    print ("Question 1 :")
+    print ("Voici détérminé numériquement le vecteur u : ")
+    print(U)
+    print("Fin de la partie 3.3")
+    print("Fin de la partie 3, merci de votre attention.")
+
     
-def K(x,t):
-    return 2
-
-def f(x):
-    return (np.cos(np.pi*x/2)-8/np.pi)
-
-a=-1
-b=1
-
-def V(x):
-    return np.cos(np.pi*x/2)
-
-
